2eeee2f5ae82a1d02f8920092000ed86c1d5c7fd
Dev Srivastava <63705608+devpra1999@users.noreply.github.com>
2023-05-17 11:13:49 +0200

    Changes to open data files on any computer with the repo

--- R Scripts/Getting Data.R
+++ R Scripts/Getting Data.R
@@ -5,28 +5,28 @@ library(dplyr)
 
 #LONG TERM BOND DATA------------------------------------------------
 #Getting the data for German 10 year bonds
-Y_G_10 <- read.csv("~/Economics/TP-IRP/Data_Files/Germany-long-bond.csv",
+Y_G_10 <- read.csv("./Data_Files/Germany-long-bond.csv",
                    skip = 6,col.names = c("Date","Yield","Type"))
 Y_G_10$Date <- ym(Y_G_10$Date)
 plot(Y_G_10$Date,Y_G_10$Yield, type = "l",ylab = "Yield",
      xlab = "Date", main = "Yields for 10 year bond - Germany")
 
 #Getting the data for French 10 year bonds
-Y_F_10 <- read.csv("~/Economics/TP-IRP/Data_Files/France-long-bond.csv",
+Y_F_10 <- read.csv("./Data_Files/France-long-bond.csv",
                    skip = 6,col.names = c("Date","Yield","Type"))
 Y_F_10$Date <- ym(Y_F_10$Date)
 plot(Y_F_10$Date,Y_F_10$Yield, type = "l",ylab = "Yield",
      xlab = "Date", main = "Yields for 10 year bond - France")
 
 #Getting the data for Italian 10 year bonds
-Y_I_10 <- read.csv("~/Economics/TP-IRP/Data_Files/Italy-long-bond.csv",
+Y_I_10 <- read.csv("./Data_Files/Italy-long-bond.csv",
                    skip = 6,col.names = c("Date","Yield","Type"))
 Y_I_10$Date <- ym(Y_I_10$Date)
 plot(Y_I_10$Date,Y_I_10$Yield, type = "l",ylab = "Yield",
      xlab = "Date", main = "Yields for 10 year bond - Italy")
 
 #Getting the data for Spanish 10 year bonds
-Y_S_10 <- read.csv("~/Economics/TP-IRP/Data_Files/Spain-long-bond.csv",
+Y_S_10 <- read.csv("./Data_Files/Spain-long-bond.csv",
                    skip = 6,col.names = c("Date","Yield","Type"))
 Y_S_10$Date <- ym(Y_S_10$Date)
 plot(Y_S_10$Date,Y_S_10$Yield, type = "l",ylab = "Yield",
@@ -35,13 +35,18 @@ plot(Y_S_10$Date,Y_S_10$Yield, type = "l",ylab = "Yield",
 
 #SHORT TERM BOND DATA------------------------------------------------------
 #Getting data for short-term rates
-Y_st <- read.csv("~/Economics/TP-IRP/Data_Files/Short-term-rates.csv")
+#Market rates
+Y_st <- read.csv("./Data_Files/Short-term-rates.csv")
 Y_st <- Y_st %>% filter(int_rt == "IRT_M3" & geo == "EA") %>% select(TIME_PERIOD,OBS_VALUE)
 colnames(Y_st) <- c("Date","Rate")
 Y_st$Date <- ym(Y_st$Date)
-#Y_st <- read.csv("~/Economics/TP-IRP/Data_Files/ECB-rates.csv", skip = 6
-                 , col.names = c("Date","Rate","Type"))
+
+#Official rates
+#Y_st <- read.csv("./Data_Files/ECB-rates.csv", skip = 6
+#                 , col.names = c("Date","Rate","Type"))
 #Y_st$Date <- ymd(Y_st$Date)
+
+#Plot short term rates
 plot(Y_st$Date, Y_st$Rate, type = "l", ylab = "Rate", xlab = "Date",
      main = "Short Term Rates for the Euro Area")
 
