1960e31c4d3c93f950e18505a757fcae683df2e2
Dev Srivastava <63705608+devpra1999@users.noreply.github.com>
2023-05-19 02:28:32 +0200

    Added plot for comparison of term premia estimates for the different countries

--- R Scripts/main.R
+++ R Scripts/main.R
@@ -21,7 +21,8 @@ for (z in 1:length(long_yield_list)){
   #Make and save the plots for the different countries in separate files
   png(paste("./Plots/Yield_decomposition_",country_list[z],".png",sep=""))
   plot(df$Date,df$Yield, type = "l", ylab = "Yield & Composition", xlab = "Date",
-       main = paste("Decomposition of yield for",country_list[z],sep = " "), ylim = c(min(df$Yield, na.rm = TRUE)-2,max(df$Yield,na.rm = TRUE)))
+       main = paste("Decomposition of yield for",country_list[z],sep = " "),
+       ylim = c(min(df$Yield, na.rm = TRUE)-2,max(df$Yield,na.rm = TRUE)))
   lines(df$Date,df$TP, lty = "dashed", col = "red",lwd = 2)
   lines(df$Date,(df$Yield - df$TP), lty = "dashed", col = "blue")
   lines(df$Date,rep(0,length(df$Date)), lty = "dotted")
@@ -34,6 +35,29 @@ for (z in 1:length(long_yield_list)){
          lwd = c(1,2,1,2)
   )
   dev.off()
+  if (z==1){
+    TP_df <- as.data.frame(cbind(df$Date,df$TP))
+  }
+  else{
+    TP_df <- cbind(TP_df,df$TP)
+  }
 }
+colnames(TP_df) <- c("Date","Germany","France","Spain","Italy")
+TP_df$Date <- as.Date(TP_df$Date)
+png("./Plots/Term Premia estimates for all the countries.png")
+plot(TP_df$Date,TP_df$Germany, type = "l", col = "blue", lwd = 2, 
+     main = "Term Premia Estimates", ylab = "Term Premia", xlab = "Date",
+     ylim = c(min(TP_df$Germany, na.rm = TRUE)-2,max(df$Yield,na.rm = TRUE)+4)
+)
+lines(TP_df$Date,TP_df$France, col = "brown", lwd = 2)
+lines(TP_df$Date,TP_df$Spain, col = "green", lwd = 2)
+lines(TP_df$Date,TP_df$Italy, col = "red", lwd = 2)
+legend("topright",
+       legend = c("Germany","France","Spain","Italy"),
+       col = c("blue","brown","green","red"),
+       lwd = c(2,2,2,2)
+)
+dev.off()
+
 
 
