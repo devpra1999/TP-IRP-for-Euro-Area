fb128a85b4feeae3a1c24e23ce31b3bbd29ecbb7
Dev Srivastava <63705608+devpra1999@users.noreply.github.com>
2023-06-15 10:17:49 +0200

    latest

--- R Scripts/Plots.R
+++ R Scripts/Plots.R
@@ -13,7 +13,7 @@ Yield_Plot <- highchart() %>%
 
 
 Germany_Term_Premia <- highchart() %>%
-  hc_add_series(master_df, "line", hcaes(x = Date, y = Spread_Germany), name = "Term Spread") %>%
+  hc_add_series(master_df, "line", hcaes(x = Date, y = Yield_Germany), name = "Yield") %>%
   hc_add_series(master_df, "line", hcaes(x = Date, y = TP_Germany),
                 name = "Term Premia", dashStyle = "dash", color = "red", lineWidth = 2) %>%
   hc_add_series(master_df, "scatter", hcaes(x = Date, y = TP_cf_Germany),
@@ -25,7 +25,7 @@ Germany_Term_Premia <- highchart() %>%
   hc_exporting(enabled = TRUE)
 
 France_Term_Premia <- highchart() %>%
-  hc_add_series(master_df, "line", hcaes(x = Date, y = Spread_France), name = "Term Spread") %>%
+  hc_add_series(master_df, "line", hcaes(x = Date, y = Yield_France), name = "Yield") %>%
   hc_add_series(master_df, "line", hcaes(x = Date, y = TP_France),
                 name = "Term Premia", dashStyle = "dash", color = "red", lineWidth = 2) %>%
   hc_add_series(master_df, "scatter", hcaes(x = Date, y = TP_cf_France),
@@ -37,10 +37,10 @@ France_Term_Premia <- highchart() %>%
   hc_exporting(enabled = TRUE)
 
 Spain_Term_Premia <- highchart() %>%
-  hc_add_series(master_df, "line", hcaes(x = Date, y = Spread_Spain), name = "Term Spread") %>%
+  hc_add_series(master_df, "line", hcaes(x = Date, y = Yield_Spain), name = "Yield") %>%
   hc_add_series(master_df, "line", hcaes(x = Date, y = TP_Spain),
                 name = "Term Premia", dashStyle = "dash", color = "red", lineWidth = 2) %>%
-  hc_add_series(master_df, "scatter", hcaes(x = Date, y = TP_cf_Spain),
+  hc_add_series(master_df, "line", hcaes(x = Date, y = TP_cf_Spain),
                 name = "Consensus_TP", marker = list(symbol = "circle", lineWidth = 0, radius = 2)) %>%
   hc_title(text = "Spain") %>%
   hc_xAxis(type = "datetime", title = list(text = "Date")) %>%
@@ -49,7 +49,7 @@ Spain_Term_Premia <- highchart() %>%
   hc_exporting(enabled = TRUE)
   
 Italy_Term_Premia <- highchart() %>%
-  hc_add_series(master_df, "line", hcaes(x = Date, y = Spread_Italy), name = "Term Spread") %>%
+  hc_add_series(master_df, "line", hcaes(x = Date, y = Yield_Italy), name = "Yield") %>%
   hc_add_series(master_df, "line", hcaes(x = Date, y = TP_Italy),
                 name = "Term Premia", dashStyle = "dash", color = "red", lineWidth = 2) %>%
   hc_add_series(master_df, "scatter", hcaes(x = Date, y = TP_cf_Italy),
